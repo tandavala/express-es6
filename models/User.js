6d1894bd608f9ecb13c622cc21b318c4ca2e123e
tandavala <jose.tandavala@gmail.com>
2020-02-07 10:37:48 +0100

    adding the User model

---
+++ models/User.js
@@ -0,0 +1,13 @@
+export default class User {
+  constructor(name, username, email) {
+    this.name = name;
+    this.username = username;
+    this.email = email;
+  }
+  getUsername() {
+    return this.username;
+  }
+  getName() {
+    return this.name;
+  }
+}
